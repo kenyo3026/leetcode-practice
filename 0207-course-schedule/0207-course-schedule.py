9f85b4e47aa93e4bbd18aac2ffe5393651632cf0
kenyo3026 <kenyo3026@gmail.com>
2026-01-19 19:48:36 +0800

    Time: 0 ms (100%), Space: 20.3 MB (23.26%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,46 +1,21 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        """
-        Determines if all courses can be completed by detecting cycles in a directed graph
-        using DFS with Three-Color Marking.
-
-        State Definitions:
-        - 0 (Unvisited): The node has never been inspected.
-        - 1 (Visiting):  The node is currently in the recursion stack (processing started but not finished).
-        - 2 (Visited):   The node and all its descendants have been fully processed and verified as safe.
-
-        Key Logic:
-        1. Cycle Detection (State == 1):
-           If we encounter a node marked '1', it means we have looped back to an ancestor 
-           in the current recursion path. This confirms a cycle exists (A -> B -> ... -> A).
-
-        2. Pruning / Memoization (State == 2):
-           If we encounter a node marked '2', it means this node was already checked in a 
-           previous DFS iteration and proved safe. We return False immediately to skip 
-           redundant computations (Optimization).
-        """
-        graph = [[] for course in range(numCourses)]
+        graph = [[] for _ in range(numCourses)]
+        in_degrees = [0] * numCourses
         for course, prereq in prerequisites:
-            graph[course].append(prereq)
+            graph[prereq].append(course)
+            in_degrees[course] += 1
 
-        visited = [0] * numCourses
+        queue = [course for course in range(numCourses) if in_degrees[course]==0]
 
-        def dfs(node): # treat as check is cycle or not
-            if visited[node] == 1:
-                return True
-            elif visited[node] == 2:
-                return False
-
-            visited[node] = 1
+        topo_sorted = []
+        while queue:
+            node = queue.pop()
+            topo_sorted.append(node)
 
             for neighbor in graph[node]:
-                if dfs(neighbor):
-                    return True
+                in_degrees[neighbor] -= 1
+                if in_degrees[neighbor] == 0:
+                    queue.append(neighbor)
 
-            visited[node] = 2
-            return False
-
-        for course in range(numCourses):
-            if dfs(course):
-                return False
-        return True
+        return len(topo_sorted) == numCourses
