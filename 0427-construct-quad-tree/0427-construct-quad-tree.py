1bd3b1a831d438170b404bc06ac3a990f5ec0632
kenyo3026 <kenyo3026@gmail.com>
2026-01-21 17:27:45 +0800

    Time: 100 ms (24.7%), Space: 20.3 MB (15.26%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.py
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -12,7 +12,9 @@ class Node:
 
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
-        def build(size, row, col): 
+
+        def build(size, row, col):
+
             if size == 1:
                 return Node(grid[row][col], True, None, None, None, None)
             
@@ -21,11 +23,13 @@ class Solution:
             topright = build(half, row, col+half)
             bottomleft = build(half, row+half, col)
             bottomright = build(half, row+half, col+half)
+
             if (
                 topleft.isLeaf and topright.isLeaf and bottomleft.isLeaf and bottomright.isLeaf and 
                 topleft.val == topright.val == bottomleft.val == bottomright.val
-                ):
+            ):
                 return Node(topleft.val, True, None, None, None, None)
+
             return Node(1, False, topleft, topright, bottomleft, bottomright)
 
         return build(len(grid), 0, 0)
