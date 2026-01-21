a6d73f970466a373304e0b077fa0edc29d9ff3fc
kenyo3026 <kenyo3026@gmail.com>
2026-01-21 15:38:38 +0800

    Time: 126 ms (86.26%), Space: 31.9 MB (64.58%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,14 +6,16 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        self.good_nodes = 0
+        self.good_counts = 0
 
         def dfs(node, max_so_far):
+
             if not node:
                 return
 
             if node.val >= max_so_far:
-                self.good_nodes += 1
+                self.good_counts += 1
+
             max_so_far = max(max_so_far, node.val)
 
             if node.left:
@@ -22,4 +24,4 @@ class Solution:
                 dfs(node.right, max_so_far)
 
         dfs(root, float('-inf'))
-        return self.good_nodes
+        return self.good_counts
