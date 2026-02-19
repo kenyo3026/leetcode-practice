466ccf2b5e3b410e1d5af644f1ee30f37b508516
kenyo3026 <kenyo3026@gmail.com>
2026-02-19 14:32:50 +0800

    Time: 4 ms (42.97%), Space: 20.2 MB (48.67%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -3,7 +3,7 @@ class Solution:
         n = len(triangle)
 
         for i in range(n-2, -1, -1):
-            for j in range(i+1):
-                triangle[i][j] += min(triangle[i+1][j:j+2])
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
 
         return triangle[0][0]
