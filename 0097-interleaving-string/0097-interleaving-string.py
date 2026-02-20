3d419aa9346cd4afbaa4e8b0376ed38260c55969
kenyo3026 <kenyo3026@gmail.com>
2026-02-20 14:16:50 +0800

    Time: 55 ms (30.26%), Space: 19.5 MB (61%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -8,15 +8,15 @@ class Solution:
         dp = [[False for _ in range(n2+1)] for _ in range(n1+1)]
         dp[0][0] = True
 
-        for i in range(1, n1 + 1):
+        for i in range(1, n1+1):
             dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
 
-        for j in range(1, n2 + 1):
+        for j in range(1, n2+1):
             dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
 
         for i in range(1, n1+1):
             for j in range(1, n2+1):
                 dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or \
-                            (dp[i][j-1] and s2[j-1] == s3[i+j-1])
-            
+                        (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+
         return dp[-1][-1]
