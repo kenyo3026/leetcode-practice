c1cb0381f8ea68552a0cca111b1da31f8ea7f071
kenyo3026 <kenyo3026@gmail.com>
2026-04-24 19:24:24 +0800

    Time: 7 ms (81.18%), Space: 19.5 MB (94.9%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,4 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = [token.lower() for token in s if token.isalpha() or token.isdigit()]
-        return s == s[::-1]
+        left, right = 0, len(s) - 1
+
+        while left < right:
+
+            if not s[left].isalpha() and not s[left].isdigit():
+                left += 1
+                continue
+
+            if not s[right].isalpha() and not s[right].isdigit():
+                right -= 1
+                continue
+
+            if s[left].lower() == s[right].lower():
+                left += 1
+                right -= 1
+            else:
+                return False
+
+        return True
