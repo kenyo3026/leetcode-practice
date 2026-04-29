1c1352e14579c619b6e34635f6b32bdc3309f95a
kenyo3026 <kenyo3026@gmail.com>
2026-04-29 22:09:00 +0800

    Time: 0 ms (100%), Space: 19.5 MB (39.37%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,38 +1,37 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = {}
+
+        graphs = {}
         for (numer, denom), value in zip(equations, values):
-            graph[numer] = graph.get(numer, {})
-            graph[numer][denom] = value
+            if not numer in graphs:
+                graphs[numer] = {}
+            graphs[numer][denom] = value
 
-            graph[denom] = graph.get(denom, {})
-            graph[denom][numer] = 1 / value
-
+            if not denom in graphs:
+                graphs[denom] = {}
+            graphs[denom][numer] = 1 / value
 
         def dfs(numer, denom, visited):
-
-            if not numer in graph or not denom in graph:
+            if not numer in graphs or not denom in graphs:
                 return -1
 
-            if denom in graph[numer]:
-                return graph[numer][denom]
+            if denom in graphs[numer]:
+                return graphs[numer][denom]
 
             visited.add(numer)
 
-            for factor in graph[numer]:
-
+            for factor in graphs[numer]:
                 if factor in visited:
                     continue
 
-                res = dfs(factor, denom, visited)
-                if res != -1:
-                    return graph[numer][factor] * res
-
+                value = dfs(factor, denom, visited)
+                if value != -1:
+                    return graphs[numer][factor] * value
+    
             return -1
 
-        results = []
+        res = []
         for numer, denom in queries:
-            res = dfs(numer, denom, set())
-            results.append(res)
+            res.append(dfs(numer, denom, set()))
 
-        return results
+        return res
