807c67e9d1c146277753addf879cfa872cbb3d47
kenyo3026 <kenyo3026@gmail.com>
2026-05-02 19:43:19 +0800

    Time: 63 ms (20.36%), Space: 40.5 MB (9.05%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,15 +1,21 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         nums = set(nums)
+        visited = set()
         max_seq = 0
 
         for num in nums:
 
+            if num in visited:
+                continue
+
             if not num - 1 in nums:
                 seq_so_far = 1
+                visited.add(num)
 
                 while num + seq_so_far in nums:
                     seq_so_far += 1
+                    visited.add(num + seq_so_far)
 
                 max_seq = max(max_seq, seq_so_far)
 
