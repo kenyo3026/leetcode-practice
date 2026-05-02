90a7609f87cfcaf0152647328579ab10cc444d01
kenyo3026 <kenyo3026@gmail.com>
2026-05-02 12:50:07 +0800

    Time: 15 ms (22.14%), Space: 19.5 MB (57.32%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -1,15 +1,17 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        nums1_set, nums2_set = set(nums1), set(nums2)
-        n1, n2 = len(nums1), len(nums2)
-        diff1, diff2 = set(), set()
+        n1, nums1_hash = len(nums1), {num:0 for num in nums1}
+        n2, nums2_hash = len(nums2), {num:0 for num in nums2}
 
         for i in range(max(n1, n2)):
 
-            if i < n1 and not nums1[i] in nums2_set and not nums1[i] in diff1:
-                diff1.add(nums1[i])
+            if i < n1 and not nums1[i] in nums2_hash:
+                nums1_hash[nums1[i]] += 1
 
-            if i < n2 and not nums2[i] in nums1_set and not nums2[i] in diff2:
-                diff2.add(nums2[i])
+            if i < n2 and not nums2[i] in nums1_hash:
+                nums2_hash[nums2[i]] += 1
 
-        return [list(diff1), list(diff2)]
+        return [
+            [num for num, count in nums1_hash.items() if count > 0],
+            [num for num, count in nums2_hash.items() if count > 0],
+        ]
