4812ada9c7a24a4ef637f9a5061e9cfc3b87ac88
kenyo3026 <kenyo3026@gmail.com>
2026-05-02 12:31:39 +0800

    Time: 1 ms (93.45%), Space: 20.1 MB (35.09%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -5,11 +5,12 @@ class Solution:
         reach_so_far = 0
         step_so_far = reach_so_far
 
-        for i in range(n - 1):
+        for i in range(n):
             reach_so_far = max(reach_so_far, i + nums[i])
 
             if i == step_so_far:
-                step += 1
                 step_so_far = reach_so_far
+                if i != n - 1:
+                    step += 1
 
         return step
