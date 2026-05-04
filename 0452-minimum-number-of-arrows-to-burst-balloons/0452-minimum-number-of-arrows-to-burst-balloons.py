cce23bf69d065954e26d5c30ddc919e0e3e0a38f
kenyo3026 <kenyo3026@gmail.com>
2026-05-04 16:03:03 +0800

    Time: 58 ms (93.32%), Space: 53.5 MB (65.49%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -5,8 +5,11 @@ class Solution:
         arrow_at = points[0][1]
 
         for xs, xe in points[1:]:
-            if xs > arrow_at:
-                arrow_so_far += 1
-                arrow_at = xe
+
+            if xs <= arrow_at:
+                continue
+
+            arrow_so_far += 1
+            arrow_at = xe
 
         return arrow_so_far
