547f51781fbc174f0fbdf4546d0650b3e2176337
kenyo3026 <kenyo3026@gmail.com>
2026-05-04 18:00:34 +0800

    Time: 35 ms (26.97%), Space: 26.8 MB (33.71%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,9 +6,7 @@ class Solution:
 
         for i in range(1, n):
             prefix_sums[i] = prefix_sums[i-1] * nums[i-1]
-
-            i = n - 1 - i
-            suffix_sums[i] = suffix_sums[i+1] * nums[i+1]
+            suffix_sums[n-i-1] = suffix_sums[n-i] * nums[n-i]
 
         producted = []
         for i in range(n):
