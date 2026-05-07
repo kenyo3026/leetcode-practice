8c08a55d5541f6a3c0f6b721f402ab092b369847
kenyo3026 <kenyo3026@gmail.com>
2026-05-07 17:53:44 +0800

    Time: 264 ms (42.24%), Space: 38.1 MB (96.55%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.py
+++ 3660-jump-game-ix/3660-jump-game-ix.py
@@ -1,18 +1,20 @@
 class Solution:
     def maxValue(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        ans = [0] * n
 
-        pre_max = [nums[0]] * n
+        prefix_max = [nums[0]] * n
         for i in range(1, n):
-            pre_max[i] = max(pre_max[i-1], nums[i])
+            prefix_max[i] = max(prefix_max[i-1], nums[i])
 
-        suf_min = float('inf')
+        suffix_min = [nums[-1]] * n
+        for i in range(n-2, -1, -1):
+            suffix_min[i] = min(suffix_min[i+1], nums[i+1])
+
+        jumped = [0] * n
         for i in range(n-1, -1, -1):
-            if pre_max[i] > suf_min:
-                ans[i] = ans[i+1]
+            if prefix_max[i] > suffix_min[i]:
+                jumped[i] = jumped[i+1] if i+1 < n else prefix_max[i]
             else:
-                ans[i] = pre_max[i]
-            suf_min = min(suf_min, nums[i])
+                jumped[i] = prefix_max[i]
 
-        return ans
+        return jumped
