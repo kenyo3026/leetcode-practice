4ff1dab757af00da2b8072785f62afbfd18b622d
kenyo3026 <kenyo3026@gmail.com>
2026-05-07 15:38:03 +0800

    Time: 7 ms (50.78%), Space: 21.6 MB (19.75%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -2,20 +2,26 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
         visited = set()
-        province_so_far = 0
+        provinces_so_far = 0
 
         def dfs(city_id):
             visited.add(city_id)
 
             for neighbor_id in range(n):
-                _is_connected = isConnected[city_id][neighbor_id]
 
-                if _is_connected and not neighbor_id in visited:
+                if neighbor_id == city_id:
+                    continue
+
+                if neighbor_id in visited:
+                    continue
+
+                if isConnected[city_id][neighbor_id] == 1:
                     dfs(neighbor_id)
+
 
         for city_id in range(n):
             if not city_id in visited:
-                province_so_far += 1
+                provinces_so_far += 1
                 dfs(city_id)
 
-        return province_so_far
+        return provinces_so_far
