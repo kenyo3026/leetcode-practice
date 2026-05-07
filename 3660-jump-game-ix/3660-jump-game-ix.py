eeb7f710e082f182b5d0bfd67f6535fb2faaca21
kenyo3026 <kenyo3026@gmail.com>
2026-05-07 18:02:11 +0800

    Time: 238 ms (56.9%), Space: 39.5 MB (71.55%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.py
+++ 3660-jump-game-ix/3660-jump-game-ix.py
@@ -6,15 +6,15 @@ class Solution:
         for i in range(1, n):
             prefix_max[i] = max(prefix_max[i-1], nums[i])
 
-        suffix_min = [nums[-1]] * n
+        suffix_min = [float('inf')] * n
         for i in range(n-2, -1, -1):
             suffix_min[i] = min(suffix_min[i+1], nums[i+1])
 
-        jumped = [0] * n
+        jumped = [0] * (n+1)
         for i in range(n-1, -1, -1):
             if prefix_max[i] > suffix_min[i]:
-                jumped[i] = jumped[i+1] if i+1 < n else prefix_max[i]
+                jumped[i] = jumped[i+1]
             else:
                 jumped[i] = prefix_max[i]
 
-        return jumped
+        return jumped[:n]
