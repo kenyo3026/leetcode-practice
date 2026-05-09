b403d4e55cab8f94c9d6a1496e1115372a159f2e
kenyo3026 <kenyo3026@gmail.com>
2026-05-09 19:42:53 +0800

    Time: 110 ms (87.14%), Space: 19.7 MB (67.14%) - LeetHub

---
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.py
@@ -0,0 +1,46 @@
+class Solution:
+    def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
+
+        def extract_layer(l):
+            top, bottom, left, right = l, m-1-l, l, n-1-l
+            vector = []
+
+            for i in range(left, right):
+                vector.append(grid[top][i])
+
+            for i in range(top, bottom):
+                vector.append(grid[i][right])
+
+            for i in range(right, left, -1):
+                vector.append(grid[bottom][i])
+
+            for i in range(bottom, top, -1):
+                vector.append(grid[i][left])
+
+            return vector
+
+        def apply_layer(l, vector):
+            top, bottom, left, right = l, m-1-l, l, n-1-l
+            vector = iter(vector)
+
+            for i in range(left, right):
+                grid[top][i] = next(vector)
+
+            for i in range(top, bottom):
+                grid[i][right] = next(vector)
+
+            for i in range(right, left, -1):
+                grid[bottom][i] = next(vector)
+
+            for i in range(bottom, top, -1):
+                grid[i][left] = next(vector)
+
+        for layer in range(min(m, n) // 2):
+            vector = extract_layer(layer)
+            _k = k % len(vector)
+            vector = vector[_k:] + vector[:_k]
+
+            apply_layer(layer, vector)
+
+        return grid
