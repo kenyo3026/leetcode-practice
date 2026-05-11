13f6f9d9ab4275e783278ced6a0fc5b8d4fd0f27
kenyo3026 <kenyo3026@gmail.com>
2026-05-11 15:37:43 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,15 +1,13 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        MOD = 10**9 + 7
+        MOD = 10 ** 9 + 7
 
         full = [0] * (n + 1)
         part = [0] * (n + 1)
 
-        full[0] = 1
-        full[1] = 1
-
-        for i in range(2, n + 1):
-            full[i] = (full[i-1] + full[i-2] + 2 * part[i-1]) % MOD
-            part[i] = (full[i-2] + part[i-1]) % MOD
+        full[0], full[1] = 1, 1
+        for i in range(2, n+1):
+            full[i] = full[i-1] + full[i-2] + 2 * part[i-1]
+            part[i] = full[i-2] + part[i-1]
 
         return full[-1]
