6e9e49a108672bc661723202bb8c38815d4a6423
kenyo3026 <kenyo3026@gmail.com>
2026-05-12 15:23:26 +0800

    Time: 7 ms (23.35%), Space: 19.1 MB (97.06%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -2,14 +2,12 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wordDict = set(wordDict)
         n = len(s)
-        dp = [True] + [False] * (n)
-
-        def find_segpoints(curr_point):
-            return [i for i in range(curr_point) if dp[i] == True]
+        dp = [True] + [False] * n
 
         for j in range(n+1):
-            segpoints = find_segpoints(j)
+            segpoints = [i for i in range(j) if dp[i]==True]
             for i in segpoints:
                 if s[i:j] in wordDict:
                     dp[j] = True
+
         return dp[-1]
