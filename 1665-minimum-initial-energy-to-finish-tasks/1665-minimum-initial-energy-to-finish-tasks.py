d3664205568073b378f7c2918a6a7c35ba49fa0b
kenyo3026 <kenyo3026@gmail.com>
2026-05-12 14:42:05 +0800

    Time: 183 ms (24.75%), Space: 57 MB (6.93%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -1,19 +1,16 @@
+import heapq
+
 class Solution:
     def minimumEffort(self, tasks: List[List[int]]) -> int:
-        # tasks.sort(key=lambda x: x[1]-x[0], reverse=True)
-        tasks_sortby_buffer = [(-(task[1]-task[0]), task[0], task[1]) for task in tasks]
-        heapq.heapify(tasks_sortby_buffer)
+        heap = [(-(m - a), a, m) for a, m in tasks]
+        heapq.heapify(heap)
 
-        total_energy_consumed_so_far = 0
-        min_initial_energy_so_far = 0
+        consumed = 0
+        min_initial = 0
 
-        while tasks_sortby_buffer:
-            buffer, actual, minimum = heapq.heappop(tasks_sortby_buffer)
-            buffer = -buffer
-            total_energy_consumed_so_far += actual
-            min_initial_energy_so_far = max(
-                min_initial_energy_so_far,
-                total_energy_consumed_so_far + buffer,
-            )
+        while heap:
+            _, actual, minimum = heapq.heappop(heap)
+            consumed += actual
+            min_initial = max(min_initial, consumed - actual + minimum)
 
-        return min_initial_energy_so_far
+        return min_initial
