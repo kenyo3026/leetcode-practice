a5aca8668c3ece1a849e4e9ffd44d20002ff1c6d
kenyo3026 <kenyo3026@gmail.com>
2026-05-17 17:53:38 +0800

    Time: 7 ms (97.28%), Space: 26.5 MB (71.44%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -2,23 +2,18 @@ class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         n = len(arr)
         queue = deque([start])
-        visited = set()
+        visited = {start}
 
         while queue:
             idx = queue.popleft()
 
+            if arr[idx] == 0:
+                return True
+
             for next_idx in (idx - arr[idx], idx + arr[idx]):
 
-                if not (0 <= next_idx < n):
-                    continue
-
-                if next_idx in visited:
-                    continue
-
-                if arr[next_idx] == 0:
-                    return True
-
-                queue.append(next_idx)
-                visited.add(next_idx)
+                if (0 <= next_idx < n) and not next_idx in visited:
+                    queue.append(next_idx)
+                    visited.add(next_idx)
 
         return False
