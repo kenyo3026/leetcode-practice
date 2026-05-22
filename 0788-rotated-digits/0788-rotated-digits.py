42ff388e641cfe3e5728248e21462b51b79cd44f
kenyo3026 <kenyo3026@gmail.com>
2026-05-22 09:39:40 +0800

    Time: 44 ms (34.1%), Space: 19.2 MB (93.61%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotatedDigits(self, n: int) -> int:
+        bad_mask = (1 << 3) | (1 << 4) | (1 << 7)
+        good_mask = (1 << 2) | (1 << 5) | (1 << 6) | (1 << 9)
+
+        good_found = 0
+        for num in range(1, n+1):
+            mask = 0
+            for char in str(num):
+                mask |= (1 << int(char))
+
+            if not (mask & bad_mask) and (mask & good_mask):
+                good_found += 1
+
+        return good_found
