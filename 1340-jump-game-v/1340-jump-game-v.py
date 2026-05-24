32868c2afe43225ca67633953e89c5a83c9e8bca
kenyo3026 <kenyo3026@gmail.com>
2026-05-24 13:02:34 +0800

    Time: 154 ms (83.71%), Space: 19.2 MB (99.71%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.py
+++ 1340-jump-game-v/1340-jump-game-v.py
@@ -3,22 +3,21 @@ class Solution:
         n = len(arr)
         dp = [1] * n
 
-        # sort indices by height ascending so dp[j] is ready before computing dp[i] 
-        # (i depends on shorter neighbors)
-        indices = sorted(range(n), key=lambda i: arr[i])
+        idxes = sorted(range(n), key=lambda x: arr[x])
 
-        for i in indices:
+        for i in idxes:
 
-            for j in range(i+1, min(n, i + d + 1)):
+            for j in range(i+1, min(n, i+d+1)):
                 if arr[i] > arr[j]:
                     dp[i] = max(dp[i], dp[j]+1)
                 else:
                     break
 
-            for j in range(i-1, max(-1, i - d - 1), -1):
+            for j in range(i-1, max(-1, i-d-1), -1):
                 if arr[i] > arr[j]:
                     dp[i] = max(dp[i], dp[j]+1)
                 else:
                     break
+
 
         return max(dp)
