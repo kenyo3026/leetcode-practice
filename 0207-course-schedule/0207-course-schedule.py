0d0620740710cb5a4bdbd0a19c068e86c99a0edb
kenyo3026 <kenyo3026@gmail.com>
2026-05-24 17:29:46 +0800

    Time: 3 ms (87.27%), Space: 21 MB (40.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,23 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        in_degress = [0 for i in range(numCourses)]
-        graph = [[] for i in range(numCourses)]
-
-        for prereq, course in prerequisites:
-            in_degress[course] += 1
+        graph = [[] for _ in range(numCourses)]
+        for course, prereq in prerequisites:
             graph[prereq].append(course)
 
-        topos = []
-        queue = deque([i for i in range(numCourses) if in_degress[i] == 0])
+        states = [0] * numCourses
 
-        while queue:
-            prereq = queue.popleft()
-            topos.append(prereq)
+        def dfs(course):
+            if states[course] == 1:
+                return False
+            elif states[course] == 2:
+                return True
 
-            for course in graph[prereq]:
-                in_degress[course] -= 1
+            states[course] = 1
 
-                if in_degress[course] == 0:
-                    queue.append(course)
+            for neighbor in graph[course]:
+                if not dfs(neighbor):
+                    return False
 
-        return topos.__len__() == numCourses
+            states[course] = 2
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
