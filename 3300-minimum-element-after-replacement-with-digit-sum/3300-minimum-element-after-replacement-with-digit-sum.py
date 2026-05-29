d4a2b16834acd8ddcb9705e8ab233c953fa80159
kenyo3026 <kenyo3026@gmail.com>
2026-05-29 09:36:55 +0800

    Time: 0 ms (100%), Space: 19.3 MB (21.61%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -3,6 +3,7 @@ class Solution:
         min_replaced = float('inf')
         for num in nums:
             replaced = 0
+
             while num != 0:
                 num, mod = num //10, num % 10
                 replaced += mod
