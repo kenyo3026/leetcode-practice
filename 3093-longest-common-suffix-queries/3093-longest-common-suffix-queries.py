7b8808ae77bfabf9bec8b08e479449a0c54879e2
kenyo3026 <kenyo3026@gmail.com>
2026-05-29 10:13:27 +0800

    Time: 1593 ms (36.66%), Space: 180.8 MB (34.44%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
@@ -1,35 +1,45 @@
 class Solution:
-    def stringIndices(self, wordsContainer, wordsQuery):
-        class Node:
-            __slots__ = ('children', 'best_idx', 'best_len')
-            def __init__(self):
+    def stringIndices(self, wordsContainer: List[str], wordsQuery: List[str]) -> List[int]:
+        class TrieNode:
+            def __init__(self, depth=1):
                 self.children = {}
-                self.best_idx = -1
-                self.best_len = float('inf')
+                self.best_idx_so_far = -1
+                self.best_len_so_far = float('inf')
 
-        root = Node()
+        class Trie:
+            def __init__(self, depth=1):
+                self.root = TrieNode()
 
-        def update(node, idx, length):
-            if length < node.best_len:
-                node.best_len = length
-                node.best_idx = idx
+            def insert(self, word, idx):
+                node = self.root
+                if (lw:=len(word)) < node.best_len_so_far:
+                    node.best_idx_so_far = idx
+                    node.best_len_so_far = lw
 
-        for i, w in enumerate(wordsContainer):
-            node = root
-            update(node, i, len(w))
-            for c in reversed(w):
-                if c not in node.children:
-                    node.children[c] = Node()
-                node = node.children[c]
-                update(node, i, len(w))
+                for char in reversed(word):
+                    if not char in node.children:
+                        node.children[char] = TrieNode()
+                    node = node.children[char]
+                    if (lw:=len(word)) < node.best_len_so_far:
+                        node.best_idx_so_far = idx
+                        node.best_len_so_far = lw
 
-        ans = []
-        for q in wordsQuery:
-            node = root
-            for c in reversed(q):
-                if c in node.children:
-                    node = node.children[c]
-                else:
-                    break
-            ans.append(node.best_idx)
-        return ans
+            def check(self, word):
+                node = self.root
+                for char in reversed(word):
+                    if char in node.children:
+                        node = node.children[char]
+                    else:
+                        break
+                return node
+
+        trie = Trie()
+
+        for i, word in enumerate(wordsContainer):
+            trie.insert(word, i)
+
+        matches = []
+        for word in wordsQuery:
+            matches.append(trie.check(word).best_idx_so_far)
+
+        return matches
