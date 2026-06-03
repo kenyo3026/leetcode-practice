8fe732889d77ffbfe2ccea0eb1f6b891250eee44
kenyo3026 <kenyo3026@gmail.com>
2026-06-03 09:46:40 +0800

    Time: 11 ms (67.05%), Space: 19.4 MB (57.8%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
@@ -1,22 +1,15 @@
 class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
         n_land, n_water = len(landStartTime), len(waterStartTime)
-        min_needed = float('inf')
+        min_both_finish = float('inf')
 
-        # Land -> Water
-        land_finish = min(
-            start + duration for start, duration in zip(landStartTime, landDuration)
-        )
-        for w in range(n_water):
-            land_first_finish = max(land_finish, waterStartTime[w]) + waterDuration[w]
-            min_needed = min(min_needed, land_first_finish)
+        def f(min_both_finish, start_time_1, duration_1, start_time_2, duration_2):
+            finish_1 = min([start_1 + dur_1 for start_1, dur_1 in zip(start_time_1, duration_1)])
+            for start_2, dur_2 in zip(start_time_2, duration_2):
+                min_both_finish = min(min_both_finish, max(finish_1, start_2) + dur_2)
 
-        # Water -> Land
-        water_finish = min(
-            start + duration for start, duration in zip(waterStartTime, waterDuration)
-        )
-        for l in range(n_land):
-            water_first_finish = max(water_finish, landStartTime[l]) + landDuration[l]
-            min_needed = min(min_needed, water_first_finish)
+            return min_both_finish
 
-        return min_needed
+        min_both_finish = f(min_both_finish, landStartTime, landDuration, waterStartTime, waterDuration)
+        min_both_finish = f(min_both_finish, waterStartTime, waterDuration, landStartTime, landDuration)
+        return min_both_finish
