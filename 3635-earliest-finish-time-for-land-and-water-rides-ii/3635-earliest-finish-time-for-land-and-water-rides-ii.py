cbcbe3d3805d1cd259084e7b8dfe3bc168e063cc
kenyo3026 <kenyo3026@gmail.com>
2026-06-03 10:05:48 +0800

    Time: 119 ms (67.27%), Space: 33.4 MB (90.91%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.py
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.py
@@ -5,11 +5,9 @@ class Solution:
 
         def f(min_both_finish, start_time_1, duration_1, start_time_2, duration_2):
             finish_1 = min([start_1 + dur_1 for start_1, dur_1 in zip(start_time_1, duration_1)])
-            for start_2, dur_2 in zip(start_time_2, duration_2):
-                min_both_finish = min(min_both_finish, max(finish_1, start_2) + dur_2)
+            return min(max(finish_1, start_2) + dur_2 for start_2, dur_2 in zip(start_time_2, duration_2))
 
-            return min_both_finish
-
-        min_both_finish = f(min_both_finish, landStartTime, landDuration, waterStartTime, waterDuration)
-        min_both_finish = f(min_both_finish, waterStartTime, waterDuration, landStartTime, landDuration)
-        return min_both_finish
+        return min(
+            f(min_both_finish, landStartTime, landDuration, waterStartTime, waterDuration),
+            f(min_both_finish, waterStartTime, waterDuration, landStartTime, landDuration),
+        )
