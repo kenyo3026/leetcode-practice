3a01ac217ec9dbef6daf96465d256fb57a248ca8
kenyo3026 <kenyo3026@gmail.com>
2026-06-05 00:55:07 +0800

    Time: 270 ms (70.12%), Space: 19.2 MB (69.73%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
@@ -1,15 +1,16 @@
 class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
-        def waviness(x: int) -> int:
-            digits = [int(d) for d in str(x)]
-            if len(digits) < 3:
-                return 0
+
+        def waviness(num):
+            digits = str(num)
+            n = len(digits)
+
             count = 0
-            for i in range(1, len(digits) - 1):
-                if digits[i] > digits[i-1] and digits[i] > digits[i+1]:
-                    count += 1  # peak
-                elif digits[i] < digits[i-1] and digits[i] < digits[i+1]:
-                    count += 1  # valley
+            for i in range(1, n-1):
+                if (digits[i-1] < digits[i] and digits[i] > digits[i+1]) or \
+                    (digits[i-1] > digits[i] and digits[i] < digits[i+1]):
+                    count += 1
+
             return count
 
-        return sum(waviness(x) for x in range(num1, num2 + 1))
+        return sum(waviness(num) for num in range(num1, num2+1))
