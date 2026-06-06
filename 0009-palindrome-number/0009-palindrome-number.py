c381f20bfe76326318ed65479f69b5c72154c502
kenyo3026 <kenyo3026@gmail.com>
2026-06-06 12:24:45 +0800

    Time: 3 ms (88.89%), Space: 19.2 MB (53.67%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,9 +4,9 @@ class Solution:
             return False
 
         x = str(x)
-        left, right = 0, -1
+        left, right = 0, len(x) - 1
 
-        while left <= len(x) // 2:
+        while left < right:
             if x[left] != x[right]:
                 return False
 
