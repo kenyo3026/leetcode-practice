82d5838e1c78cc5f6e88715f10c829fca11f59e6
kenyo3026 <kenyo3026@gmail.com>
2026-06-07 12:55:19 +0800

    Time: 125 ms (82.93%), Space: 28.1 MB (25.41%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -7,26 +7,20 @@
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
         tree_dict = {}
-
-        for parent, _, _ in descriptions:
-
-            if not parent in tree_dict:
-                node = TreeNode(val=parent)
-                tree_dict[parent] = node
-
         child_set = set()
+
         for parent, child, is_left in descriptions:
 
-            child_set.add(child)
-
+            if not parent in tree_dict:
+                tree_dict[parent] = TreeNode(parent)
             if not child in tree_dict:
-                node = TreeNode(val=child)
-                tree_dict[child] = node
+                tree_dict[child] = TreeNode(child)
 
             if is_left:
                 tree_dict[parent].left = tree_dict[child]
             else:
                 tree_dict[parent].right = tree_dict[child]
 
-        root = next(iter(tree_dict.keys() - child_set))
-        return tree_dict[root]
+            child_set.add(child)
+
+        return tree_dict[next(iter(tree_dict.keys() - child_set))]
