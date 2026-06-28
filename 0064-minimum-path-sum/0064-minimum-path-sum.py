7d9ae48845dd75234d8e93c2f34c5e54cbb08293
kenyo3026 <kenyo3026@gmail.com>
2026-06-28 14:17:06 +0800

    Time: 9 ms (65.37%), Space: 21.5 MB (70.56%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -10,9 +10,6 @@ class Solution:
 
         for i in range(1, n):
             for j in range(1, m):
-                if grid[i-1][j] < grid[i][j-1]:
-                    grid[i][j] += grid[i-1][j]
-                else:
-                    grid[i][j] += grid[i][j-1]
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
 
         return grid[-1][-1]
