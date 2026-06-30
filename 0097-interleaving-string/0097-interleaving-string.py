8a86425511f597d70c46a28f930f3a28758c7cfd
kenyo3026 <kenyo3026@gmail.com>
2026-06-30 20:08:11 +0800

    Time: 56 ms (28.58%), Space: 19.4 MB (72.96%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -4,18 +4,24 @@ class Solution:
         if n1 + n2 != n3:
             return False
 
-        dp = [[False for _ in range(n2+1)] for _ in range(n1+1)]
+        dp = [[False for j in range(n2+1)] for i in range(n1+1)]
         dp[0][0] = True
 
         for i in range(1, n1+1):
             dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
+            # if not s1[i] == s3[i]:
+            #     break
+            # dp[i][0] = True
 
         for j in range(1, n2+1):
             dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
+            # if not s2[j] == s3[i]:
+            #     break
+            # dp[0][j] = True
 
         for i in range(1, n1+1):
             for j in range(1, n2+1):
-                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or \
-                        (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+                dp[i][j] = (dp[i-1][j] and s1[i-1]==s3[i+j-1]) or \
+                        (dp[i][j-1] and s2[j-1]==s3[i+j-1])
 
         return dp[-1][-1]
