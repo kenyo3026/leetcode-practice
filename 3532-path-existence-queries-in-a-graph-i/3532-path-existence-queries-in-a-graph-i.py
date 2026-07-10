007b5b73a726357c609fa0269cf3ce05215d1316
kenyo3026 <kenyo3026@gmail.com>
2026-07-10 13:49:20 +0800

    Time: 99 ms (57.26%), Space: 50.6 MB (28.23%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -1,11 +1,12 @@
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
 
-        cluster_id = 0
-        cluster = [0 for i in range(n)]
+        clr_id = 0
+        clrs = [0 for _ in range(n)]
         for i in range(1, n):
             if nums[i] - nums[i-1] > maxDiff:
-                cluster_id += 1
-            cluster[i] = cluster_id
+                clr_id += 1
+            clrs[i] = clr_id
 
-        return [(cluster[u] == cluster[v]) for u, v in queries]
+        print(clrs)
+        return [clrs[u] == clrs[v] for u, v in queries]
