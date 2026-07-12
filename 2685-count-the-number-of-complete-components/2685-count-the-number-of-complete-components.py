b46f6c1fd36b4739a129759548cb98a08de59b37
kenyo3026 <kenyo3026@gmail.com>
2026-07-12 13:45:16 +0800

    Time: 67 ms (22.05%), Space: 19.7 MB (90.68%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -1,29 +1,46 @@
 class Solution:
-    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+    def countCompleteComponents(self, n: int, edges: list[list[int]]) -> int:
         parent = list(range(n))
+        rank = [0] * n
         degree = [0] * n
 
-        def find(x):
+        def find(x: int) -> int:
             while parent[x] != x:
-                parent[x] = parent[parent[x]]
+                parent[x] = parent[parent[x]]  # path compression
                 x = parent[x]
             return x
 
+        def union(x: int, y: int) -> None:
+            root_x, root_y = find(x), find(y)
+            if root_x == root_y:
+                return
+            # union by rank
+            if rank[root_x] < rank[root_y]:
+                root_x, root_y = root_y, root_x
+            parent[root_y] = root_x
+            if rank[root_x] == rank[root_y]:
+                rank[root_x] += 1
+
         for u, v in edges:
-            ru, rv = find(u), find(v)
-            if ru != rv:
-                parent[ru] = rv
+            union(u, v)
             degree[u] += 1
             degree[v] += 1
 
-        cnt = [0] * n
-        deg_sum = [0] * n
-        for i in range(n):
-            r = find(i)
-            cnt[r] += 1
-            deg_sum[r] += degree[i]
+        node_count = [0] * n
+        degree_sum = [0] * n
 
-        return sum(
-            cnt[r] and deg_sum[r] // 2 == cnt[r] * (cnt[r] - 1) // 2
-            for r in range(n)
-        )
+        for i in range(n):
+            root = find(i)
+            node_count[root] += 1
+            degree_sum[root] += degree[i]
+
+        answer = 0
+        for root in range(n):
+            if node_count[root] == 0:
+                continue
+            cnt = node_count[root]
+            edge_count = degree_sum[root] // 2
+            if edge_count == cnt * (cnt - 1) // 2:
+                answer += 1
+
+        return answer
