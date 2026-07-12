48056749d93c852dcf6a306c80690f7c4351c671
kenyo3026 <kenyo3026@gmail.com>
2026-07-12 13:37:14 +0800

    Time: 15 ms (11.79%), Space: 20.3 MB (69.99%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -9,11 +9,15 @@ class Solution:
             return parents[x]
 
         def union(x, y):
-            parents[find(x)] = find(y)
+            x = find(x)
+            y = find(y)
+
+            if x != y:
+                parents[y] = x
 
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(n):
                 if isConnected[i][j] == 1:
                     union(i, j)
 
-        return sum(1 for i in range(n) if find(i) == i)
+        return sum(1 for x in range(n) if find(x) == x)
